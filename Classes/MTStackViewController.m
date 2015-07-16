f69ce80be3f45c8d641f3c05c86fe7a5ac54c162
Rendy Pranata <rpranata@moneytree.jp>
2015-07-16 17:49:38 +1000

    interface orientation is deprecated.

--- Classes/MTStackViewController.m
+++ Classes/MTStackViewController.m
@@ -1119,7 +1119,7 @@ const char *MTStackViewControllerKey = "MTStackViewControllerKey";
 - (CGRect)screenBounds
 {
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
-    if (UIInterfaceOrientationIsLandscape([self interfaceOrientation]))
+    if (UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]))
         screenBounds.size = CGSizeMake(screenBounds.size.height, screenBounds.size.width);
     
     return screenBounds;
